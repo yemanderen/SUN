c6d6cb21fda17f71f8b287c7500b313c172065f0
Sun William (Consultant) <william.sun@consultant.volvo.com>
2021-11-18 21:51:18 +0800

    hi

---
+++ DjangoWebProject1/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\a031489.VCN\source\repos\SunSoft\DjangoWebProject1\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

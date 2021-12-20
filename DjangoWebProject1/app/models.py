67c73f4b335ed0e90f8f0910847c9ba9c2db7195
“yemanderen” <ultrasuno@hotmail.com>
2021-12-20 15:23:26 +0800

    gg

--- DjangoWebProject1/app/models.py
+++ DjangoWebProject1/app/models.py
@@ -5,3 +5,12 @@ Definition of models.
 from django.db import models
 
 # Create your models here.
+class The_user(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'test user'
+
+        def __str__(self):
+            return self.username

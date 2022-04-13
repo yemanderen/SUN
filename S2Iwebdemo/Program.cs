4c0b253447c5383b7e3844bb34dd6e9375c7649f
野蛮的人 <ultrasuno@hotmail.com>
2022-04-13 19:31:38 +0800

    Update Program.cs
    w

--- S2Iwebdemo/Program.cs
+++ S2Iwebdemo/Program.cs
@@ -21,7 +21,6 @@ namespace S2Iwebdemo
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
-        
+                });        
     }
 }
